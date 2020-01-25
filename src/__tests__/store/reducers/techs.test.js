59de02ce7cfe4da1a4d326374a725bb9715f465f
Henrique Lira <henrique.lira99@gmail.com>
2020-01-25 11:34:32 -0300

    Add coverage report

--- src/__tests__/store/reducers/techs.test.js
+++ src/__tests__/store/reducers/techs.test.js
@@ -2,6 +2,12 @@ import reducer, { INITAL_STATE } from '~/store/modules/tech/reducer';
 import * as Techs from '~/store/modules/tech/actions';
 
 describe('Techs reducer', () => {
+    it('DEFAULT', () => {
+        const state = reducer(undefined, {});
+
+        expect(state).toStrictEqual(INITAL_STATE);
+    });
+
     it('ADD_TECH', () => {
         const state = reducer(INITAL_STATE, Techs.addTech('Node.js'));
 
